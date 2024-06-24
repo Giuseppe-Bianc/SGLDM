8256b5f1edfbb4a36573d410c9810d03b42c3e06
GIUSEPPE BIANCONI <75339264+Giuseppe-Bianc@users.noreply.github.com>
2024-06-24 16:09:15 +0200

    Update Dependencies.cmake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,39 +5,77 @@ include(cmake/CPM.cmake)
 # targets
 function(SGLDM_setup_dependencies)
 
-  # For each dependency, see if it's
-  # already been provided to us by a parent project
+    if (NOT TARGET glm::glm)
+        CPMAddPackage(
+                NAME glm
+                GIT_REPOSITORY https://github.com/g-truc/glm.git
+                GIT_TAG master # Use "master" for the latest version
+                OPTIONS # Add options if needed
+                "GLM_TEST_ENABLE OFF" # Disable tests if needed
+                "GLM_ENABLE_CXX_20 ON"
+                "GLM_ENABLE_SIMD_AVX2 ON"
+        )
+    endif ()
 
-  if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
-  endif()
+    # For each dependency, see if it's
+    # already been provided to us by a parent project
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.11.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
+    if (NOT TARGET fmtlib::fmtlib)
+        CPMAddPackage("gh:fmtlib/fmt#10.2.1")
+    endif ()
 
-  if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
-  endif()
+    if (NOT TARGET spdlog::spdlog)
+        if (WIN32)
+            CPMAddPackage(
+                    NAME
+                    spdlog
+                    VERSION
+                    1.14.1
+                    GITHUB_REPOSITORY
+                    "gabime/spdlog"
+                    OPTIONS
+                    "SPDLOG_FMT_EXTERNAL ON"
+                    "SPDLOG_ENABLE_PCH ON"
+                    "SPDLOG_BUILD_PIC ON"
+                    "SPDLOG_WCHAR_SUPPORT ON"
+                    "SPDLOG_WCHAR_FILENAMES ON"
+                    "SPDLOG_SANITIZE_ADDRESS ON")
+        else ()
+            CPMAddPackage(
+                    NAME
+                    spdlog
+                    VERSION
+                    1.14.1
+                    GITHUB_REPOSITORY
+                    "gabime/spdlog"
+                    OPTIONS
+                    "SPDLOG_FMT_EXTERNAL ON"
+                    "SPDLOG_ENABLE_PCH ON"
+                    "SPDLOG_BUILD_PIC ON"
+                    "SPDLOG_SANITIZE_ADDRESS ON")
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
-  endif()
+        endif ()
+    endif ()
 
-  if(NOT TARGET ftxui::screen)
+    if (NOT TARGET Catch2::Catch2WithMain)
+        CPMAddPackage("gh:catchorg/Catch2@3.6.0")
+    endif ()
+
+    if (NOT TARGET CLI11::CLI11)
+        CPMAddPackage("gh:CLIUtils/CLI11@2.3.2")
+    endif ()
+
+    if(NOT TARGET ftxui::screen)
+                    "gabime/spdlog"
     cpmaddpackage("gh:ArthurSonzogni/FTXUI@5.0.0")
+                    OPTIONS
   endif()
+                    "SPDLOG_FMT_EXTERNAL ON"
 
+                    "SPDLOG_ENABLE_PCH ON"
   if(NOT TARGET tools::tools)
+                    "SPDLOG_BUILD_PIC ON"
     cpmaddpackage("gh:lefticus/tools#update_build_system")
+                    "SPDLOG_WCHAR_SUPPORT ON"
   endif()
-
 endfunction()
