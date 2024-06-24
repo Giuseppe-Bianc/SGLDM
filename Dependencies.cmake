f730a0d875814be3b5dd4e6c9b9b390174d8602a
GIUSEPPE BIANCONI <75339264+Giuseppe-Bianc@users.noreply.github.com>
2024-06-24 18:30:09 +0200

    Update Dependencies.cmake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -24,58 +24,34 @@ function(SGLDM_setup_dependencies)
         CPMAddPackage("gh:fmtlib/fmt#10.2.1")
     endif ()
 
-    if (NOT TARGET spdlog::spdlog)
-        if (WIN32)
-            CPMAddPackage(
-                    NAME
-                    spdlog
-                    VERSION
-                    1.14.1
-                    GITHUB_REPOSITORY
-                    "gabime/spdlog"
-                    OPTIONS
-                    "SPDLOG_FMT_EXTERNAL ON"
-                    "SPDLOG_ENABLE_PCH ON"
-                    "SPDLOG_BUILD_PIC ON"
-                    "SPDLOG_WCHAR_SUPPORT ON"
-                    "SPDLOG_WCHAR_FILENAMES ON"
-                    "SPDLOG_SANITIZE_ADDRESS ON")
-        else ()
-            CPMAddPackage(
-                    NAME
-                    spdlog
-                    VERSION
-                    1.14.1
-                    GITHUB_REPOSITORY
-                    "gabime/spdlog"
-                    OPTIONS
-                    "SPDLOG_FMT_EXTERNAL ON"
-                    "SPDLOG_ENABLE_PCH ON"
-                    "SPDLOG_BUILD_PIC ON"
-                    "SPDLOG_SANITIZE_ADDRESS ON")
+   if(NOT TARGET spdlog::spdlog)
+    cpmaddpackage(
+      NAME
+      spdlog
+      VERSION
+      1.11.0
+      GITHUB_REPOSITORY
+      "gabime/spdlog"
+      OPTIONS
+      "SPDLOG_FMT_EXTERNAL ON"
+      "SPDLOG_ENABLE_PCH ON"
+      "SPDLOG_BUILD_PIC ON")
+  endif()
 
-        endif ()
-    endif ()
+  if(NOT TARGET Catch2::Catch2WithMain)
+    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+  endif()
 
-    if (NOT TARGET Catch2::Catch2WithMain)
-        CPMAddPackage("gh:catchorg/Catch2@3.6.0")
-    endif ()
+  if(NOT TARGET CLI11::CLI11)
+    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+  endif()
 
-    if (NOT TARGET CLI11::CLI11)
-        CPMAddPackage("gh:CLIUtils/CLI11@2.3.2")
-    endif ()
-
-    if(NOT TARGET ftxui::screen)
-                    "gabime/spdlog"
+  if(NOT TARGET ftxui::screen)
     cpmaddpackage("gh:ArthurSonzogni/FTXUI@5.0.0")
-                    OPTIONS
   endif()
-                    "SPDLOG_FMT_EXTERNAL ON"
 
-                    "SPDLOG_ENABLE_PCH ON"
   if(NOT TARGET tools::tools)
-                    "SPDLOG_BUILD_PIC ON"
     cpmaddpackage("gh:lefticus/tools#update_build_system")
-                    "SPDLOG_WCHAR_SUPPORT ON"
   endif()
+
 endfunction()
