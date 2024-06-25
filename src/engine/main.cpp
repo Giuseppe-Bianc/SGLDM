0135c016682cb6f2711f0074431cdec078702aa3
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2024-06-25 16:32:33 +0200

    update

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -42,8 +42,8 @@ int main(int argc, const char **argv) {
         }
 
         // Set GLFW context version and profile
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         // Create a GLFW window
